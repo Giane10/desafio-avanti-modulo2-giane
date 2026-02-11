70d47e044271a6d13c86cc4b29f84a7e82196b2d
Giane Costa <giane_costaa@outlook.com>
2026-02-11 01:44:28 -0300

    security

--- v1-legado-al2/variables.tf
+++ v1-legado-al2/variables.tf
@@ -3,5 +3,5 @@ variable "meu_ip_publico" {
     description = "Endereço IP público para o Security Group SSH"
     # IMPORTANTE: Substitua pelo seu endereço IP público
     # Para saber o seu IP público, acesse https://www.whatismyip.com/
-    default     = "MEU_IP"
+    
 }
